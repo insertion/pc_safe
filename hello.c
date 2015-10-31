0be195849027ec2e8e3882a1b126ff2dcfeb984f
wanglei <398758695@qq.com>
2015-10-31 18:50:27 +0800

    new file

---
+++ hello.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+extern my_print(char *);
+int main( int argc,char **argv)
+{
+	int addr;
+	void (*test)(char *); 
+	test=0x40100a;//函数指针的值为我们打印的my_print函数的地址
+    
+	my_print("hello\n");
+	__asm
+	{
+		    mov eax,[esp-4]
+		    mov dword ptr [addr],eax
+	};
+	printf("hello的地址是： %d\n",addr);
+	printf("我们可以输出这个地址的内容： %s\n",addr);
+	printf("mian:    0x%x\nprintf:  0x%x\nmy_print:0x%x\n",main,printf,my_print);
+	
+	//test("\n0x40100a 确实是my_print的地址\n");//测试我们输出的函数地址是否正确
+	return 0;
+}
---
+++ myprint.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+
+void my_print(char *s)
+{
+    printf(s);
+    return;
+}

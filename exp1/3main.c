4acdc50810c85d48a85a01f597d379b3c256dfa8
wanglei <398758695@qq.com>
2015-11-09 19:09:15 +0800

    change main

--- exp1/3main.c
+++ exp1/3main.c
@@ -1,9 +1,36 @@
 #include<stdio.h>
 #include<stdlib.h>
+extern int sum(int);
 int main(int argc,char **argv)
-{
+{	
+	char string[]= "sum = %d";
 	int i=100;
 	if(argc>1)
 	i = atoi(argv[1]);
-	printf("sum(100) = %d",sum(i));
+
+	__asm
+	{
+		push [i]
+		call sum
+		add esp,4
+		
+		push eax
+		lea eax,[string]
+		push  eax
+		call DWORD PTR printf
+		add  esp,8
+
+	
+	}
+	return 0;
 }
+//asm zhong buneng yong precode
+//	printf("sum(100) = %d",sum(i));
+
+	//用 _emit 定义字节。如：
+//_asm _emit 0x90
+//或
+//_asm
+//{
+//    _emit 0x90
+//}

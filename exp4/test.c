710f6e7f726b35b6383da2c1591452ff2f99d8dc
wang lei <398758695@qq.com>
2015-12-14 19:39:10 +0800

    step2

--- exp4/test.c
+++ exp4/test.c
@@ -1,6 +1,7 @@
 #include <windows.h>
 #include<stdio.h>
 #pragma comment(lib, "user32.lib")
+int addr;
 BOOL CompStr(LPSTR s1, LPSTR s2)
 {
     PCHAR p, q;
@@ -15,8 +16,11 @@ DWORD GetIAFromImpDesc(DWORD dwBase, LPCSTR lpszName,PIMAGE_IMPORT_DESCRIPTOR pI
     PIMAGE_THUNK_DATA pthunk, pthunk2;
     PIMAGE_IMPORT_BY_NAME pOrdinalName;
     if (pImpDesc->Name == 0) return 0;
+    //如果导入表的第一元素的名字为0，则没有dll
+    //第一个dll为user32.dll
     pthunk = (PIMAGE_THUNK_DATA) (dwBase + pImpDesc->OriginalFirstThunk);
     pthunk2 = (PIMAGE_THUNK_DATA) (dwBase + pImpDesc->FirstThunk);
+    
     for (; pthunk->u1.Function != 0; pthunk++, pthunk2++) {
         if (pthunk->u1.Ordinal & 0x80000000) continue;
         pOrdinalName = (PIMAGE_IMPORT_BY_NAME) (dwBase + pthunk->u1.AddressOfData);
@@ -34,12 +38,40 @@ DWORD GetIAFromImportTable(DWORD dwBase, LPCSTR lpszFuncName)
  /*4*/   DWORD dwRVAImpTbl = pOptHeader->DataDirectory[1].VirtualAddress;
  /*5*/   PIMAGE_IMPORT_DESCRIPTOR pImpTbl =( PIMAGE_IMPORT_DESCRIPTOR) ((DWORD)dwBase + dwRVAImpTbl);
  return GetIAFromImpDesc(dwBase,lpszFuncName,pImpTbl);
-} 
+}
+    char *s="hello";
+    char *h="l'm hacked";
+__declspec(naked) MyMessageBoxA()
+{
+    //MessageBoxA(NULL, "l'm hackded!", "msg", MB_OK);
+    //printf("l'am hacked!\n");
+     __asm
+    {
+        mov eax,h
+        mov [esp+12],eax
+        //修改第二个参数为l'm hacked
+        jmp addr
+        //这里栈平衡非常重要
+    }
+}
+
 void main()
 {
+     int op;
      DWORD dwBaseAddress  = (DWORD)GetModuleHandle(0);
      //获得PE结构的基地址
-     DWORD entryPoint = GetIAFromImportTable(dwBaseAddress,"MessageBoxA");
-     printf("[1]:function entryPoint is 0x%08x\n",entryPoint);
+     DWORD *entryPoint = (DWORD *)GetIAFromImportTable(dwBaseAddress,"MessageBoxA");
+     if(entryPoint==NULL)
+     {
+         printf("[error]:MessageBoxA doesn't was called !\n");
+     }
+     else
+      printf("[1]:MessageBoxA entryPoint is %08p ==>0x%08x\n",entryPoint,*entryPoint);
+     
+     VirtualProtect(entryPoint,4,PAGE_EXECUTE_READWRITE,&op);
+     addr=*entryPoint;
+     (*entryPoint)=(int)MyMessageBoxA;
+     // printf("[2]:function entryPoint is %08p ==>0x%08x\n",MyMessageBoxA,*MyMessageBoxA);
+     // MyMessageBoxA();
      MessageBoxA(NULL, "hello", "msg", MB_OK);
 } 
